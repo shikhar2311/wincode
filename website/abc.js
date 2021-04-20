756bea933f848f9b07fe0c050521afe1557014b2
shikhar2311 <79536546+shikhar2311@users.noreply.github.com>
2021-04-20 21:56:09 +0530

    Add files via upload

---
+++ website/abc.js
@@ -0,0 +1,5 @@
+AOS.init({
+    offset: 400, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 3000 // values from 0 to 3000, with step 50ms
+  });
