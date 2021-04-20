ea7bb87ddc6a9f44e8408f791402374493a71f76
shikhar2311 <79536546+shikhar2311@users.noreply.github.com>
2021-04-20 22:34:50 +0530

    Delete website directory

--- website/abc.js
+++
@@ -1,5 +0,0 @@
-AOS.init({
-    offset: 400, // offset (in px) from the original trigger point
-    delay: 100, // values from 0 to 3000, with step 50ms
-    duration: 3000 // values from 0 to 3000, with step 50ms
-  });
